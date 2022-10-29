a4ece2fa9378280e66a7c118b4b87dd4ab71a063
Krishna <libraryismyparadise@gmail.com>
2022-10-29 15:45:10 +0545

    Insert data in respective tables

--- data.sql
+++ data.sql
@@ -13,3 +13,23 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'June 7, 2005', 7, true, '20.4');
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'October 13, 1998', 3, true, '17');
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, true, '22');
+
+/* query multiple tables*/
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animal SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animal SET species_id = 2 WHERE name NOT LIKE '%mon';
+
+UPDATE animal SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animal SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animal SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animal SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
